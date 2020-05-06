fb649d3a5c88f0587705bde6ed13011564af163a
Venen185 <63510242+Venen185@users.noreply.github.com>
2020-05-06 14:01:33 +0300

    Update EnemyRespawnPoint.cs

--- Assets/Scripts/EnemyRespawnPoint.cs
+++ Assets/Scripts/EnemyRespawnPoint.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class EnemyRespawnPoint : MonoBehaviour
 {
     [SerializeField] public GameObject _enemy;
